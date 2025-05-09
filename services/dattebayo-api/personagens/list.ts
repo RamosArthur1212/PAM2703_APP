e00e45ac98b4efd89afeac4cbdafcf311d10b90d
Arthur Ramos <arthur.ramos9@etec.sp.gov.br>
2025-05-08 22:11:17 -0300

    aula api

---
+++ services/dattebayo-api/personagens/list.ts
@@ -0,0 +1,17 @@
+const API_HOST =  'https://dattebayo-api.onrender.com'
+
+export async function fetchPersonagens(){
+    try{
+        const response = await fetch(`${API_HOST}/characters`)
+
+        if(!response.ok){
+            throw new Error(`Erro na requisição: ${response.statusText}`)
+        }
+
+       return await response.json();
+
+    }catch(error){
+        console.log(`Erro ao buscar personagens: ${error}`)
+        return [];
+    }
+}
